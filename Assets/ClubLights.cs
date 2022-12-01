ea01bbd4208b45735eec8f279acef289e1eb8612
Jmart01 <jonricmart01@gmail.com>
2022-12-01 16:02:30 -0600

    fixed lights, need to still fix animations so that they are correct with sword hand/ shield hand, FINAL BUILD MADE HERE 2022

--- Assets/ClubLights.cs
+++ Assets/ClubLights.cs
@@ -5,10 +5,17 @@ public class ClubLights : MonoBehaviour
     public float xAngle, yAngle, zAngle;
     public float speed;
 
+    private float xRot, yRot, zRot;
 
+    private void Start()
+    {
+        xRot = transform.eulerAngles.x;
+        yRot = transform.eulerAngles.y; 
+        zRot = transform.eulerAngles.z;
+    }
     void Update()
     {
         gameObject.transform.Rotate(xAngle * speed * Time.deltaTime, yAngle* speed * Time.deltaTime, zAngle * speed * Time.deltaTime, Space.World);
-        //cube2.transform.Rotate(xAngle, yAngle, zAngle, Space.World);
+        
     }
 }
