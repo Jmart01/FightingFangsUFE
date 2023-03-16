27689d0d042ee5db296a3bd4d1e3dd2ed501cedc
Jmart01 <jonricmart01@gmail.com>
2023-03-16 15:15:25 -0500

    still working on switching out materials for echo, worked for a bit

--- Assets/FightingFangs/Prefabs/Characters/Echo/Model/EchoMaterialChange.cs
+++ Assets/FightingFangs/Prefabs/Characters/Echo/Model/EchoMaterialChange.cs
@@ -8,6 +8,7 @@ public class EchoMaterialChange : MonoBehaviour
 {
     public GameObject bodyGrp;
     public Material[] materials;
+    int CurrentMat = 0;
     private void Awake()
     {
         UFE.OnMove += this.OnMove;
@@ -26,13 +27,21 @@ public class EchoMaterialChange : MonoBehaviour
                 //Debug.Log(move.name + player.name);
                 if(move.name == "Echo_Down_Special")
                 {
-                    //Material bodyMatToChange = bodyGrp.GetComponent<SkinnedMeshRenderer>().materials[1];
-                    //for(int CurrentMat = 0; CurrentMat< materials.Length; CurrentMat++) 
-                    //{
-                        
-                    //}
-                    
-                    //Debug.Log("should change colors");
+                    //Material bodyMatToChange = bodyGrp.GetComponent<SkinnedMeshRenderer>().materials[0];
+                    /*for(int CurrentMat = 0; CurrentMat< materials.Length; CurrentMat++) 
+                    {
+                        Debug.Log("Changing Colors");
+                    }*/
+                    if (CurrentMat >= 2)
+                    {
+                        CurrentMat = 0;
+                    }
+
+
+                    bodyGrp.GetComponent<SkinnedMeshRenderer>().materials[1] = materials[CurrentMat];
+                    Debug.Log(CurrentMat.ToString());
+                    CurrentMat++;
+
                 }
             }
         } 
