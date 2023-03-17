e470c5a40e95113a65242a356602cd6d4fa23237
Jmart01 <jonricmart01@gmail.com>
2023-03-17 14:57:26 -0500

    Got the Materials to change, working on the stance/move speed changes now, just need to add in last stance file and then few lines of code

--- Assets/FightingFangs/Prefabs/Characters/Echo/Model/EchoMaterialChange.cs
+++ Assets/FightingFangs/Prefabs/Characters/Echo/Model/EchoMaterialChange.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UFE3D;
 using System;
+using FPLibrary;
 
 public class EchoMaterialChange : MonoBehaviour
 {
     public GameObject bodyGrp;
-    public Material[] materials;
+    public Material[] mats;
     int CurrentMat = 0;
+    Fix64 _originalMoveForwardSpeed;
+    Fix64 _originalMoveBackSpeed;
     private void Awake()
     {
         UFE.OnMove += this.OnMove;
@@ -16,6 +19,15 @@ public class EchoMaterialChange : MonoBehaviour
     private void OnDisable()
     {
         UFE.OnMove -= this.OnMove;
+    }
+    private void Start()
+    {
+        ControlsScript controlscript = GetComponentInParent<ControlsScript>();
+        if (controlscript != null)
+        {
+             _originalMoveForwardSpeed = controlscript.myInfo.physics._moveForwardSpeed;
+            _originalMoveBackSpeed = controlscript.myInfo.physics._moveBackSpeed;
+        }
     }
 
     private void OnMove(MoveInfo move, ControlsScript player)
@@ -27,23 +39,45 @@ public class EchoMaterialChange : MonoBehaviour
                 //Debug.Log(move.name + player.name);
                 if(move.name == "Echo_Down_Special")
                 {
-                    //Material bodyMatToChange = bodyGrp.GetComponent<SkinnedMeshRenderer>().materials[0];
-                    /*for(int CurrentMat = 0; CurrentMat< materials.Length; CurrentMat++) 
-                    {
-                        Debug.Log("Changing Colors");
-                    }*/
-                    if (CurrentMat >= 2)
+                    CheckStance(player);
+                    
+                    SkinnedMeshRenderer renderer = bodyGrp.GetComponent<SkinnedMeshRenderer>();
+                    Material[] MeshMats = renderer.materials;
+                    CurrentMat++;
+                    if (CurrentMat > 2)
                     {
                         CurrentMat = 0;
                     }
-
-
-                    bodyGrp.GetComponent<SkinnedMeshRenderer>().materials[1] = materials[CurrentMat];
-                    Debug.Log(CurrentMat.ToString());
-                    CurrentMat++;
-
+                    MeshMats[1] = mats[CurrentMat];
+                    renderer.materials = MeshMats;
+                    //bodyGrp.GetComponent<SkinnedMeshRenderer>().materials[1] = materials[CurrentMat];
+                    Debug.Log(MeshMats[1].name);
                 }
             }
         } 
     }
+    
+    private void CheckStance(ControlsScript player)
+    {
+        UFE3D.CharacterInfo character = player.myInfo;
+        if(player.currentCombatStance == CombatStances.Stance1)
+        {
+            //Once the stance changes from stance1 to stance2, lowers the move speed
+            player.myInfo.physics._moveBackSpeed = 4.5;
+            player.myInfo.physics._moveForwardSpeed = 5.5;
+        }
+        Debug.Log("Switched Stances");
+        if(player.currentCombatStance == CombatStances.Stance2)
+        {
+            if(character != null)
+            {
+                player.myInfo.physics._moveBackSpeed = 6.5;
+                player.myInfo.physics._moveForwardSpeed = 7.5;
+            }
+        }
+        //need to do the same thing for the third combat stance
+
+    }
+    
+    
 }
